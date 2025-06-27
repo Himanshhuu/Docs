40fa488ce1392252e36e134991e163cc28fbb7cf
Himanshhuu <blitz.z0101@gmail.com>
2025-06-27 17:21:43 +0530

    Changed toolbar styles

--- src/app/documents/[documentId]/Editor.tsx
+++ src/app/documents/[documentId]/Editor.tsx
@@ -20,12 +20,14 @@ import { FontSizeExtension } from '@/extensions/font-size';
 import { LineHeightExtension } from '@/extensions/line-height';
 
 import { useEditorStore } from '@/store/use-editor-store';
+import { Ruler } from './ruler';
 
 
 export const Editor = () => {
   const {setEditor}=useEditorStore();
 
     const editor = useEditor({
+      immediatelyRender: false,
       onCreate({editor}){
         setEditor(editor);
       },
@@ -59,10 +61,7 @@ export const Editor = () => {
     extensions: [
         StarterKit,
         FontSizeExtension,
-        LineHeightExtension.configure({
-          types:["heading","paragraph"],
-          defaultLineHeight: "normal"
-        }),
+        LineHeightExtension,
         TextAlign.configure({
           types: ["heading","paragraph"]
         }),
@@ -108,6 +107,7 @@ export const Editor = () => {
   })
     return (
         <div className="size-full overflow-x-auto bg-[#f9fbfd] px-4 print:p-0 print:bg-white print:overflow-visible">
+            <Ruler/>
             <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
                 <EditorContent editor={editor} />                
             </div>
